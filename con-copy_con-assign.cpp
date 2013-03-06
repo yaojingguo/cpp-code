3682019bd7689c38bd865b37f00767d315eae8a3
Jingguo Yao <yaojingguo@gmail.com>
2013-03-06 15:39:30 +0800

    Add more checks

--- con-copy_con-assign.cpp
+++ con-copy_con-assign.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 class thing {
+  private:
+    int no;
+
   public: 
     thing() 
     {
+      no = 10;
       cout << "default constructor" << endl;
     }
     thing(const thing& other) 
     {
+      no = other.no;
       cout << "copy constructor" << endl;
     }
     thing& operator = (thing& rhs)
@@ -17,15 +22,21 @@ class thing {
       cout << "assignment operator" << endl;
       return *this;
     }
+    void info() {
+      cout << "no: " << no << endl;
+    }
 };
 
 int main(int argc, const char *argv[]) 
 {
   thing one; // constructor
+  one.info();
   thing one_ = thing(); // constructor
 
   one = one_; // assignment operator
+  one.info();
   thing two(one); // copy constructor
+  two.info();
 
   return 0;
 }
