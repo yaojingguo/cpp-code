3d49d1757ed0e8d6005022124f370b95fab24642
Jingguo Yao <yaojingguo@gmail.com>
2013-05-19 17:10:05 +0800

    Update

--- CGAL/RangeTree.cpp
+++ CGAL/RangeTree.cpp
@@ -12,16 +12,16 @@ void query(int x1, double y1, int x2, double y2) {
   std::vector<Key> InputList, OutputList;
   std::vector<Key>::iterator first, last, current;
 
-  InputList.push_back(Key(8,5.1));
-  InputList.push_back(Key(1,1.1));
-  InputList.push_back(Key(3,2.1));
+  InputList.push_back(Key(8, 5.1));
+  InputList.push_back(Key(1, 1.1));
+  InputList.push_back(Key(3, 2.1));
 
-  Range_tree_2_type Range_tree_2(InputList.begin(),InputList.end());
+  Range_tree_2_type Range_tree_2(InputList.begin(), InputList.end());
 
-  Interval win=Interval(Key(x1,y1),Key(x2,y2));
+  Interval win = Interval(Key(x1, y1), Key(x2, y2));
   Range_tree_2.window_query(win, std::back_inserter(OutputList));
-  current=OutputList.begin();
-  while(current!=OutputList.end()){
+  current = OutputList.begin();
+  while (current != OutputList.end()) {
     std::cout << (*current).x()<< "-" << (*current).y() << std::endl;
     current++;
   }
