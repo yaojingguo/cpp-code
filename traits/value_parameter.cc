c246ef8765e75afd2e18c39be109821521881e31
Jingguo Yao <yaojingguo@gmail.com>
2018-02-05 20:04:06 +0800

    update

--- traits/value_parameter.cc
+++ traits/value_parameter.cc
@@ -9,9 +9,17 @@ private:
   T v[max];
 };
 
+template <typename T, char* label>
+class X {
+
+}
+
 int main(int argc, char const *argv[]) {
   Buffer<char, 128> cbuf;
   Buffer<int, 10> ibuf;
+
+  char lx2 = "BMW-i5";
+  X<int, lx2> xx;
 
   return 0;
 }
