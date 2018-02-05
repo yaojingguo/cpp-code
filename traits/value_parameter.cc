ffa77281153b3c91883da7ee0040341fefb1db9e
Jingguo Yao <yaojingguo@gmail.com>
2018-02-05 20:07:40 +0800

    update

--- traits/value_parameter.cc
+++ traits/value_parameter.cc
@@ -4,7 +4,8 @@ using namespace std;
 template<typename T, int max>
 class Buffer {
 public:
-  Buffer() {}
+  Buffer() {
+  }
 private:
   T v[max];
 };
@@ -12,14 +13,17 @@ private:
 template <typename T, char* label>
 class X {
 
-}
+};
 
 int main(int argc, char const *argv[]) {
   Buffer<char, 128> cbuf;
   Buffer<int, 10> ibuf;
 
-  char lx2 = "BMW-i5";
-  X<int, lx2> xx;
+  // char lx2[] = "BMW323Ci";
+  // X<int, lx2> xx;
+
+  constexpr int max = 20;
+  Buffer<int, max> bm;
 
   return 0;
 }
