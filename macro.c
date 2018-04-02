e53ac95ecdf3ec129e6f85d8cc363a5b406e2827
Jingguo Yao <yaojingguo@gmail.com>
2018-04-02 18:12:34 +0800

    update

--- macro.c
+++ macro.c
@@ -20,23 +20,22 @@ int main() {
 }
 
 /*
+gcc -E produces something similar to the following text:
+
 void cool_info() {
   printf("INFO\n");
-  }
+}
 
 
-  void debug() {
-    printf("DEBUG\n");
-    }
+void debug() {
+  printf("DEBUG\n");
+}
 
-    int main() {
-      cool_info();
-        cool_info();
+int main() {
+  cool_info();
+  cool_info();
 
-          debug();
-            debug();
-            }
-
-
-
+  debug();
+  debug();
+}
 */
