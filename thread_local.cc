68a32a1c7c0ad1ea6680a00e9240674864c9931d
Jingguo Yao <yaojingguo@gmail.com>
2018-08-14 16:10:29 +0800

    update

--- thread_local.cc
+++ thread_local.cc
@@ -14,7 +14,7 @@ void make_key(void) {
 void *thread(void *ptr) {
   // Thread type
   long type = (long) ptr;
-  printf("Thread type: %ld\n", type);
+  printf("thread type: %ld\n", type);
 
   (void) pthread_once(&key_once, make_key);
 
@@ -24,44 +24,29 @@ void *thread(void *ptr) {
     local_ptr = malloc(sizeof(int));
     assert(local_ptr);
     int_ptr = (int*) local_ptr;
-    // printf("---------------\n");
-    if (type == 1) {
-      *int_ptr = 100;
-    } else if (type == 2) {
-      *int_ptr = 200;
-    }
-    // printf("===============\n");
+    *int_ptr = type * 100;
     (void) pthread_setspecific(key, local_ptr);
-    // printf("+++++++++++++++\n");
   }
 
   for (int i = 0; i < 3; i++) {
     local_ptr = pthread_getspecific(key);
-    // printf("aaaaaaaaaaaaaaaaa\n");
     assert(local_ptr);
-    // printf("bbbbbbbbbbbbbbbbb\n");
     int_ptr = (int*) local_ptr;
-    // printf("pointer: %p\n", int_ptr);
-    // printf("ccccccccccccccccc\n");
     printf("loop value: %d\n", *int_ptr);
     sleep(1);
   }
 
-  // printf("exit...\n");
   return NULL;
 }
 
 int main() {
-  // Create the thread objs
   pthread_t thread1, thread2;
   long thr1 = 1;
   long thr2 = 2;
 
-  // Start the threads
   pthread_create(&thread1, NULL, *thread, (void*) thr1);
   pthread_create(&thread2, NULL, *thread, (void*) thr2);
 
-  // Wait for the threads to finish
   pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
 
