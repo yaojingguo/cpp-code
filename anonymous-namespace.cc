816790b03facaaca54e15012ec1e16ef11479a06
Jingguo Yao <yaojingguo@gmail.com>
2019-02-10 19:53:01 +0800

    update

--- anonymous-namespace.cc
+++ anonymous-namespace.cc
@@ -4,9 +4,16 @@ namespace {
     int add(int a, int b) {
         return a + b;
     }
+    struct Person {
+        int age;
+    };
 }
 
 int main(int argc, char *argv[]) {
     printf("sum: %d\n", add(10, 20));
+
+    struct Person p = {.age= 100};
+
+    printf("person age: %d\n", p.age);
     return 0;
 }
