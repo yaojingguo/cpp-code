7c7fe4b558c9a377e2a4760a920c07abaec09c76
Jingguo Yao <yaojingguo@gmail.com>
2020-07-08 16:01:54 +0800

    update

--- return.cpp
+++ return.cpp
@@ -23,13 +23,27 @@ Point returnValue(int x, int y) {
     return p;
 }
 
-Point returnReference(int x, int y) {
+// Compiler gives the follownig warning:
+//   warning: reference to stack memory associated with local variable 'p' returned [-Wreturn-stack-address]
+Point& returnReference(int x, int y) {
+    Point p(x, y);
+    return p;
 }
 
-int main() 
-{ 
+// Compiler gives the following warning:
+//   warning: address of stack memory associated with local variable 'p' returned [-Wreturn-stack-address]
+Point* returnPointer(int x, int y) {
+    Point p(x, y);
+    return &p;
+}
+
+int main() { 
     Point value = returnValue(100, 200);
     cout << "\nresult: x = " << value.getX() << ", y = " << value.getY() << endl;
+
+    Point& reference = returnReference(100, 200);
+
+    Point* pointer = returnPointer(100, 200);
 
     return 0; 
 }
