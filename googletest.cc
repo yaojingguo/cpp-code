d1f541a3374b71c290268d14605cc488d055e6c2
Jingguo Yao <yaojingguo@gmail.com>
2021-01-28 21:17:51 +0800

    update

---
+++ googletest.cc
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+int Factorial(int n) {
+    if (n == 0) return 1;
+    return n * Factorial(n - 1);
+}
+
+// Tests factorial of 0.
+TEST(FactorialTest, HandlesZeroInput) {
+  EXPECT_EQ(Factorial(0), 1);
+}
+
+// Tests factorial of positive numbers.
+TEST(FactorialTest, HandlesPositiveInput) {
+  EXPECT_EQ(Factorial(1), 1);
+  EXPECT_EQ(Factorial(2), 2);
+  EXPECT_EQ(Factorial(3), 6);
+  EXPECT_EQ(Factorial(8), 40320);
+}
