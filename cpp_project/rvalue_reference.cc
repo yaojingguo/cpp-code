e6c3340c658f128b142ddf2d687d7b99718c8dad
Jingguo Yao <yaojingguo@gmail.com>
2021-07-03 18:08:55 +0800

    update

--- cpp_project/rvalue_reference.cc
+++ cpp_project/rvalue_reference.cc
@@ -19,9 +19,9 @@ struct Z {
    Z(const int&, const int&) {}
 };
 
-template <typename T, typename A1, typename A2>
-T* factory(A1&& a1, A2&& a2) {
-   return new T(std::forward<A1>(a1), std::forward<A2>(a2));
+template<typename T, typename A1, typename A2>
+T* factory(A1& a1, A2& a2) {
+   return new T(a1, a2);
 }
 
 // reference-overload.cpp
@@ -46,22 +46,25 @@ void f(MemoryBlock&&)
 }
 
 void one() {
-   int a = 4, b = 5;
-   W* pw = factory<W>(a, b);
-   X* px = factory<X>(2, b);
-   Y* py = factory<Y>(a, 2);
-   Z* pz = factory<Z>(2, 2);
+   // int a = 4, b = 5;
+   // W* pw = factory<W>(a, b);
+   // X* px = factory<X>(2, b);
+   // Y* py = factory<Y>(a, 2);
+   // Z* pz = factory<Z>(2, 2);
 
-   delete pw;
-   delete px;
-   delete py;
-   delete pz;
+   // delete pw;
+   // delete px;
+   // delete py;
+   // delete pz;
 }
 
 
 int main()
 {
-   MemoryBlock block;
-   f(block);
-   f(MemoryBlock());
+   // MemoryBlock block;
+   // f(block);
+   // f(MemoryBlock());
+   int a = 4, b = 5;
+   W* pw = factory<W>(a, b);
 }
+
