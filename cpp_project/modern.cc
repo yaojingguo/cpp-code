081571d0eab768965c10e75c0806387dd2ff656d
Jingguo Yao <yaojingguo@gmail.com>
2021-07-04 11:22:00 +0800

    update

--- cpp_project/modern.cc
+++ cpp_project/modern.cc
@@ -11,60 +11,94 @@ using namespace std;
 class Widget
 {
 public:
-Widget(const int size) {
-  data = std::make_unique<int>(size);
-}
-void do_something() {
+  Widget(const int size)
+  {
+    data = std::make_unique<int>(size);
+  }
+  void do_something()
+  {
+  }
 
-}
 private:
   std::unique_ptr<int> data;
 };
 
-void functionUsingWidget() {
+void functionUsingWidget()
+{
   Widget w(100);
   w.do_something();
 }
 
-void vector_usage() {
+void vector_usage()
+{
   vector<string> apples;
   apples.push_back("Pine Apple");
 }
 
-void map_usage() {
+void map_usage()
+{
   map<string, string> apple_color;
   apple_color["Pine Apple"] = "Green";
 }
 
-void multimap_usage() {
+void multimap_usage()
+{
   cout << "mulitmap usage" << endl;
   multimap<int, int> nums1{{10, 1}, {2, 3}};
-  for (auto const &x: nums1) {
+  for (auto const &x : nums1)
+  {
     cout << x.first << ": " << x.second << endl;
   }
 }
 
-void multiset_usage() {
+void multiset_usage()
+{
   cout << "multiset usage" << endl;
   multiset<int> mset{1, 2, 10, 0, 10, 5, 10};
-  for (int x: mset) {
+  for (int x : mset)
+  {
     cout << x << endl;
   }
 }
 
-void comp_usage() {
-  auto comp = [](const int v1, const int v2) 
+void comp_usage()
+{
+  auto comp = [](const int v1, const int v2)
   {
     return v1 < v2;
   };
   vector<int> vec{1, 10, 0};
   sort(vec.begin(), vec.end(), comp);
-  
+
   cout << "sorted vector" << endl;
-  for (int no: vec) {
+  for (int no : vec)
+  {
     cout << no << " ";
   }
   cout << endl;
+}
+
+struct S
+{
+  std::string name;
+  float num;
+  S(std::string s, float f) : name(s), num(f) {}
+};
+
+void uniform_initialization_usage()
+{
+  vector<S> v;
+  S s1("North", 2.7);
+  S s2("Frank", 3.5);
+  S s3("Jeri", 85.9);
+
+  v.push_back(s1);
+  v.push_back(s2);
+  v.push_back(s3);
+
+  vector<S> v2{s1, s2, s3};
+
+  std::vector<S> v3{{"Norah", 2.7}, {"Frank", 3.5}, {"Jeri", 85.9}};
 }
 
 int main(int argc, char const *argv[])
