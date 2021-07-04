c281524a1c96f0f66b56ee1f8c1bdbd7d82e5f54
Jingguo Yao <yaojingguo@gmail.com>
2021-07-04 17:23:35 +0800

    update

--- cpp_project/lvalue_reference.cc
+++ cpp_project/lvalue_reference.cc
@@ -9,18 +9,18 @@ struct Person {
   short Age;
 };
 
-int main(){
-   // Declare a Person object.
-   Person myFriend;
+int main() {
+  // Declare a Person object.
+  Person myFriend;
 
-   // Declare a reference to the Person object.
-   Person& rFriend = myFriend;
+  // Declare a reference to the Person object.
+  Person& rFriend = myFriend;
 
-   // Set the fields of the Person object.
-   // Updating either variable changes the same object.
-   myFriend.Name = "Bill";
-   rFriend.Age = 40;
+  // Set the fields of the Person object.
+  // Updating either variable changes the same object.
+  myFriend.Name = "Bill";
+  rFriend.Age = 40;
 
-   // Print the fields of the Person object to the console.
-   cout << rFriend.Name << " is " << myFriend.Age << endl;
+  // Print the fields of the Person object to the console.
+  cout << rFriend.Name << " is " << myFriend.Age << endl;
 }
