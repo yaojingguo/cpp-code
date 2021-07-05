9da3e73dc322e8d246f82cd2a812cd389500ea31
Jingguo Yao <yaojingguo@gmail.com>
2021-07-05 16:09:10 +0800

    update

--- cpp_project/file.cc
+++ cpp_project/file.cc
@@ -1,9 +1,12 @@
 #include <cstdio>
 #include <cstdlib>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main() {
+int one() {
   int is_ok = EXIT_FAILURE;
   FILE* fp = fopen("test.txt", "r");
   if (!fp) {
@@ -25,3 +28,34 @@ int main() {
   fclose(fp);
   return is_ok;
 }
+
+void two() {
+  string filename = "test.bin";
+  fstream s(filename, s.binary | s.trunc | s.in | s.out);
+
+  if (!s.is_open()) {
+    cout << "failed to open " << filename << endl;
+  } else {
+    double d = 3.14;
+    s.write(reinterpret_cast<char*>(&d), sizeof(d));
+    s << 123 << "abc";
+
+    s.seekp(0);
+    
+    s.read(reinterpret_cast<char*>(&d), sizeof(d));
+    int n;
+    string str;
+    if (s >> n >> str) {
+      cout << "read back from file: " << d << " " << n << " " << endl;
+    }
+  } 
+}
+
+int main() {
+  two();
+}
+
+// C++ file system API
+// System call
+// C I/O library
+// fstream
