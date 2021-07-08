f8f3b106e742ce3b113228a7b0e99d8e097661eb
Jingguo Yao <yaojingguo@gmail.com>
2021-07-08 15:59:09 +0800

    update

--- cpp_project/queue.h
+++ cpp_project/queue.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <list>
-using namespace std;
 
 template <typename E>
 class Queue {
@@ -13,5 +12,27 @@ class Queue {
   size_t size() const;
 
  private:
-  list<E> list_;
+  std::list<E> list_;
 };
+
+template <typename E>
+Queue<E>::Queue() {}
+
+template <typename E>
+Queue<E>::~Queue() {}
+
+template <typename E>
+void Queue<E>::Enqueue(E element) {
+  list_.push_back(element);
+}
+
+template <typename E>
+E Queue<E>::Dequeue() {
+  E front = list_.front();
+  list_.pop_front();
+}
+
+template <typename E>
+size_t Queue<E>::size() const { 
+  return list_.size(); 
+}
