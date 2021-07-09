f225fc24b5bbe1e339b5d2c631d89dcdf2da5ac5
Jingguo Yao <yaojingguo@gmail.com>
2021-07-09 14:08:07 +0800

    update

--- cpp_project/stack_trace.cc
+++ cpp_project/stack_trace.cc
@@ -1,41 +1,57 @@
-#include <execinfo.h>  // for backtrace
-#include <dlfcn.h>     // for dladdr
 #include <cxxabi.h>    // for __cxa_demangle
+#include <dlfcn.h>     // for dladdr
+#include <execinfo.h>  // for backtrace
 
-#include <string>
 #include <sstream>
+#include <string>
+#include <iostream>
 
-// A C++ function that will produce a stack trace with demangled function and method names.
-std::string Backtrace(int skip = 1)
-{
-	void *callstack[128];
-	const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
-	char buf[1024];
-	int nFrames = backtrace(callstack, nMaxFrames);
+// A C++ function that will produce a stack trace with demangled function and
+// method names.
+std::string Backtrace(int skip = 1) {
+  void *callstack[128];
+  const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
+  char buf[1024];
+  int nFrames = backtrace(callstack, nMaxFrames);
 
-	std::ostringstream trace_buf;
-	for (int i = skip; i < nFrames; i++) {
-		Dl_info info;
-		if (dladdr(callstack[i], &info)) {
-			char *demangled = NULL;
-			int status;
-			demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
-			snprintf(buf, sizeof(buf), "%-3d %*0p %s + %zd\n",
-					 i, 2 + sizeof(void*) * 2, callstack[i],
-					 status == 0 ? demangled : info.dli_sname,
-					 (char *)callstack[i] - (char *)info.dli_saddr);
-			free(demangled);
-		} else {
-			snprintf(buf, sizeof(buf), "%-3d %*0p\n",
-					 i, 2 + sizeof(void*) * 2, callstack[i]);
-		}
-		trace_buf << buf;
-	}
-	if (nFrames == nMaxFrames)
-		trace_buf << "  [truncated]\n";
-	return trace_buf.str();
+  std::ostringstream trace_buf;
+  for (int i = skip; i < nFrames; i++) {
+    Dl_info info;
+    if (dladdr(callstack[i], &info)) {
+      char *demangled = NULL;
+      int status;
+      demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
+      snprintf(buf, sizeof(buf), "%-3d %*0p %s + %zd\n", i,
+               2 + sizeof(void *) * 2, callstack[i],
+               status == 0 ? demangled : info.dli_sname,
+               (char *)callstack[i] - (char *)info.dli_saddr);
+      free(demangled);
+    } else {
+      snprintf(buf, sizeof(buf), "%-3d %*0p\n", i, 2 + sizeof(void *) * 2,
+               callstack[i]);
+    }
+    trace_buf << buf;
+  }
+  if (nFrames == nMaxFrames) trace_buf << "  [truncated]\n";
+  return trace_buf.str();
 }
 
+void a();
+void b();
+void c();
+
+void a() {
+  b();
+}
+
+void b() {
+  c();
+}
+
+void c() {
+  std::string trace = Backtrace();
+  std::cout << trace << std::endl;
+}
 int main() {
-  
+  a();
 }
